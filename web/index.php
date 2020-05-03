6cee1ebaab0c4a380bc8a67e0b99418aeb4ce430
Megan Hendrickson <dearmegs@gmail.com>
2020-05-02 21:51:39 -0500

    added landing page

--- web/index.php
+++ web/index.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport"
+     content="width=device-width, initial-scale=1, user-scalable=yes">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <title>Hello World!</title>
+</head>
+<header>
+
+</header>
+<body>
+<h1>Well, Hello There!</h1>
+<p>Allow me to introduce myslef: Im a designer-developer-in-training and a chocolate lover.
+  I like to perfect every detail, put puzzles together, laugh at silly jokes, cry over bad design, 
+  and take walks in the sunshine. I'm still figuring out what I am doing with my life. But hey, 
+  nice to meet you!
+</p>
+
+ 
+</body>
+</html>
 <?php
 
   phpinfo();
