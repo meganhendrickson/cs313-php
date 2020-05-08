c53ceb05e28d1aa71627b8e2cb41f242e9ad93f3
Megan Hendrickson <dearmegs@gmail.com>
2020-05-07 22:29:59 -0500

    update landing page

---
+++ cse341.php
@@ -0,0 +1,6 @@
+<?php include $_SERVER ['DOCUMENT_ROOT'].'/common/header.php';?>
+    <main>
+        <h1>CSE 341 Assignments</h1>
+        <p>Coming Soon!</p>
+    </main>
+<?php include $_SERVER ['DOCUMENT_ROOT'].'/common/footer.php';?> 
