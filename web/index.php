c7748cffed854b7d3de37dc069943fbccdefcafd
Megan Hendrickson <dearmegs@gmail.com>
2020-05-08 08:26:51 -0500

    fixes

--- web/index.php
+++ web/index.php
@@ -1,10 +1,14 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/common/header.php'?>
-    <main id="hello">
-        <h1>Well, Hello There!</h1>
-        <p>Allow me to introduce myself: I'm a designer, a web developer-in-training, and a chocolate lover.
-            I like to perfect every detail, put puzzles together, laugh at silly jokes, cry over bad design,
-            and take walks in the sunshine. I'm still figuring out what I'm doing with my life. But hey,
-            nice to meet you!
-        </p>
+    <main>
+        <div class="hello-image">
+            <div class="hello-text">
+                <h1>Well, Hello There!</h1>
+                <p>Allow me to introduce myself: I'm a designer, a web developer-in-training, and a chocolate lover.
+                    I like to perfect every detail, put puzzles together, laugh at silly jokes, cry over bad design,
+                    and take walks in the sunshine. I'm still figuring out what I'm doing with my life. But hey,
+                    nice to meet you!
+                </p>
+            </div>
+        </div>
     </main>
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'?>
