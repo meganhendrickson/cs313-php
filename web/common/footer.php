e8e3bab3de1dd9e61b4a8a82032408bba6b8b78d
Megan Hendrickson <dearmegs@gmail.com>
2020-05-08 08:36:59 -0500

    more fixes

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,6 +1,5 @@
     <footer>
         <p>&copy; <?php echo date("Y");?> | Megan Hendrickson | All Rights Reserved</p>
-        <p><?php echo "Last modified" . date(" F d, Y.", getlastmod()); ?></p>
         <!-- scripts -->
         <script src="https://mighty-wave-93548.herokuapp.com/js/myscripts.js"></script>
     </footer>
--- web/index.php
+++ web/index.php
@@ -1,14 +1,12 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/common/header.php'?>
-    <main>
-        <div class="hello-image">
-            <div class="hello-text">
-                <h1>Well, Hello There!</h1>
-                <p>Allow me to introduce myself: I'm a designer, a web developer-in-training, and a chocolate lover.
-                    I like to perfect every detail, put puzzles together, laugh at silly jokes, cry over bad design,
-                    and take walks in the sunshine. I'm still figuring out what I'm doing with my life. But hey,
-                    nice to meet you!
-                </p>
-            </div>
+    <div class="hello-image">
+        <div class="hello-text">
+            <h1>Well, Hello There!</h1>
+            <p>Allow me to introduce myself: I'm a designer, a web developer-in-training, and a chocolate lover.
+                I like to perfect every detail, put puzzles together, laugh at silly jokes, cry over bad design,
+                and take walks in the sunshine. I'm still figuring out what I'm doing with my life. But hey,
+                nice to meet you!
+            </p>
         </div>
-    </main>
+    </div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'?>
