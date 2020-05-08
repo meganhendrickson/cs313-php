e31e7d14e670c7ebe3267bf813994d004bc0c00b
Megan Hendrickson <dearmegs@gmail.com>
2020-05-07 23:45:07 -0500

    fixes

--- web/cse341.php
+++ web/cse341.php
@@ -1,8 +1,6 @@
 <?php include $_SERVER ['DOCUMENT_ROOT'].'/common/header.php';?>
-<div id="page-container">
     <main>
         <h1>CSE 341 Assignments</h1>
         <p>Coming Soon!</p>
     </main>
-</div>
 <?php include $_SERVER ['DOCUMENT_ROOT'].'/common/footer.php';?> 
