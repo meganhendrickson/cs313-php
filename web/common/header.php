e398e07a48d30eec8a9f0b26769cd7b7d504a856
Megan Hendrickson <dearmegs@gmail.com>
2020-05-07 23:16:10 -0500

    update navigation

--- web/common/header.php
+++ web/common/header.php
@@ -14,7 +14,7 @@
 
         <nav class="topnav" id="myTopnav">
             <a href="https://mighty-wave-93548.herokuapp.com/">Megan Hendrickson</a>
-            <a href="https://mighty-wave-93548.herokuapp.com/cse341.html">CSE 341</a>
+            <a href="https://mighty-wave-93548.herokuapp.com/cse341.php">CSE 341</a>
             <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                 <i class="fa fa-bars"></i></a>
         </nav>
