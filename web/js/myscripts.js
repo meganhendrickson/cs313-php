36d1ef3fcf5e0ad350fd002c4d2234f546934115
Megan Hendrickson <dearmegs@gmail.com>
2020-05-09 18:49:48 -0500

    build shopping display and cart

--- web/js/myscripts.js
+++ web/js/myscripts.js
@@ -1,3 +1,4 @@
+// responsive nav bar function
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,3 +7,37 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+// shopping cart functions
+function cartAction(action, product_code) {
+  var queryString = "";
+  if (action != "") {
+      switch (action) {
+      case "add":
+          queryString = 'action=' + action + '&code=' + product_code
+                  + '&quantity=' + $("#qty_" + product_code).val();
+          break;
+      case "remove":
+          queryString = 'action=' + action + '&code=' + product_code;
+          break;
+      case "empty":
+          queryString = 'action=' + action;
+          break;
+      }
+  }
+  jQuery.ajax({
+      url : "ajax-action.php",
+      data : queryString,
+      type : "POST",
+      success : function(data) {
+          $("#cart-item").html(data);
+          if (action == "add") {
+              $("#add_" + product_code + " img").attr("src",
+                      "images/icon-check.png");
+              $("#add_" + product_code).attr("onclick", "");
+          }
+      },
+      error : function() {
+      }
+  });
+}
