99346946225919c90150b6df949a90678fd57648
Megan Hendrickson <dearmegs@gmail.com>
2020-05-09 20:28:29 -0500

    add to cart

--- web/shopping/index.php
+++ web/shopping/index.php
@@ -24,7 +24,7 @@ if (! empty($productArray)) {
     foreach ($productArray as $k => $v) {
         ?>
 		<div class="product-item">
-        <form id="frmCart">
+        <form id="frmCart" method="post" action="index.php?action=add&code+<?php echo $productArray[$k]["code"];?>">
             <div class="product-image">
                 <img src="<?php echo $productArray[$k]["image"]; ?>">
             </div>
@@ -33,16 +33,9 @@ if (! empty($productArray)) {
                     <strong><?php echo $productArray[$k]["name"]; ?></strong>
                 </div>
                 <div class="product-info product-price"><?php echo "$".$productArray[$k]["price"]; ?></div>
-                <div class="product-info">
-                    <button class="add-to-cart-button" type="button"
-                        id="add_<?php echo $productArray[$k]["code"]; ?>"
-                        class="btnAddAction cart-action"
-                        onClick="cartAction('add','<?php echo $productArray[$k]["code"]; ?>')">
-                        <img src="images/add-to-cart.png" />
-                    </button>
-                    <input type="text"
-                        id="qty_<?php echo $productArray[$k]["code"]; ?>"
-                        name="quantity" value="1" size="2" />
+                <div class="cart-action">
+                    <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
+                    <input type="submit" class="addbtn" value='<img src="images/add-to-cart.png" alt="add" />'/>
                 </div>
             </div>
         </form>
