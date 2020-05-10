32c5231483e85487f7d590ff367fd6d8e72ef6d1
Megan Hendrickson <dearmegs@gmail.com>
2020-05-09 22:53:49 -0500

    updates

--- web/shopping/product.php
+++ web/shopping/product.php
@@ -7,42 +7,42 @@ class Product
             'id' => '1',
             'name' => 'Bee Kind Tshirt',
             'code' => 'bee-kind',
-            'image' => 'images/bee-kind.jpg',
+            'image' => 'product-images/bee-kind.jpg',
             'price' => '15.00'
         ),
         "chaos" => array(
             'id' => '2',
             'name' => 'Chaos Coordinator Tshirt',
             'code' => 'chaos',
-            'image' => 'images/chaos-coordinator.jpg',
+            'image' => 'product-images/chaos-coordinator.jpg',
             'price' => '15.00'
         ),
         "fine" => array(
             'id' => '3',
             'name' => 'Everythings Fine Tshirt',
             'code' => 'fine',
-            'image' => 'images/fine.jpg',
+            'image' => 'product-images/fine.jpg',
             'price' => '15.00'
         ),
         "mama-bear" => array(
             'id' => '4',
             'name' => 'Mama Bear Tshirt',
             'code' => 'mama-bear',
-            'image' => 'images/mama-bear.jpg',
+            'image' => 'product-images/mama-bear.jpg',
             'price' => '15.00'
         ),
         "shark" => array(
             'id' => '5',
             'name' => 'Shark Family Tshirt Set',
             'code' => 'shark',
-            'image' => 'images/shark-set.jpg',
+            'image' => 'product-images/shark-set.jpg',
             'price' => '45.00'
         ),
         "it-is-well" => array(
             'id' => '6',
             'name' => 'It is Well Tshirt',
             'code' => 'it-is-well',
-            'image' => 'images/it-is-well.jpg',
+            'image' => 'product-images/it-is-well.jpg',
             'price' => '15.00'
         )
     );
