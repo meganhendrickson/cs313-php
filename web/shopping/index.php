d4f6bad822f431adace72703061733c547424e29
Megan Hendrickson <dearmegs@gmail.com>
2020-05-09 21:24:04 -0500

    add to cart

--- web/shopping/index.php
+++ web/shopping/index.php
@@ -1,49 +1,20 @@
 <?php
-    ob_start();
-    include $_SERVER['DOCUMENT_ROOT'].'/common/header.php';
-    $buffer=ob_get_contents();
-    ob_end_clean();
-
-    $title = "Shopping Cart Assignment";
-    $buffer = preg_replace('/(<title>)(.*?)(<\/title>)/i', '$1' . $title . '$3', $buffer);
-
-    echo $buffer;
-?>
-<link rel="stylesheet" type="text/css" href="main.css">
-<main>
-    <h1>PHP Shopping Cart Demo</h1>
-<?php
+/* SHOPPING CONTROLLER */
+//product array 
 require_once ("product.php");
-$product = new Product();
-$productArray = $product->getAllProduct();
-?>
-<div id="product-grid">
-    <div class="txt-heading">Products</div>
-<?php
-if (! empty($productArray)) {
-    foreach ($productArray as $k => $v) {
-        ?>
-		<div class="product-item">
-        <form id="frmCart" method="post" action="index.php?action=add&code+<?php echo $productArray[$k]["code"];?>">
-            <div class="product-image">
-                <img src="<?php echo $productArray[$k]["image"]; ?>">
-            </div>
-            <div>
-                <div class="product-info">
-                    <strong><?php echo $productArray[$k]["name"]; ?></strong>
-                </div>
-                <div class="product-info product-price"><?php echo "$".$productArray[$k]["price"]; ?></div>
-                <div class="cart-action">
-                    <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
-                    <input type="submit" class="addbtn" value="add to cart"/>
-                </div>
-            </div>
-        </form>
-    </div>
-	<?php
+
+// Create or access a Session
+session_start();
+
+$action = filter_input(INPUT_POST, 'action');
+    if($action == NULL){
+        $action = filter_input(INPUT_GET, 'action');
     }
+
+switch ($action){
+    case 'something':
+    break;
+
+    default:
+        include 'view/product-display.php';
 }
-?>
-</div>
-</main>
-<?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'?>
---
+++ web/shopping/view/product-display.php
@@ -0,0 +1,45 @@
+<?php
+    ob_start();
+    include $_SERVER['DOCUMENT_ROOT'].'/common/header.php';
+    $buffer=ob_get_contents();
+    ob_end_clean();
+
+    $title = "Shopping Cart Assignment";
+    $buffer = preg_replace('/(<title>)(.*?)(<\/title>)/i', '$1' . $title . '$3', $buffer);
+
+    echo $buffer;
+?>
+
+<!-- PRODUCT DISPLAY VIEW -->
+
+<main>
+    <h1>PHP Shopping Cart Demo</h1>
+    <div id="product-grid">
+        <?php
+        if (! empty($productArray)) {
+            foreach ($productArray as $k => $v) {
+                ?>
+                <div class="product-item">
+                <form id="frmCart" method="post" action="index.php?action=add&code+<?php echo $productArray[$k]["code"];?>">
+                    <div class="product-image">
+                        <img src="<?php echo $productArray[$k]["image"]; ?>">
+                    </div>
+                    <div>
+                        <div class="product-info">
+                            <strong><?php echo $productArray[$k]["name"]; ?></strong>
+                        </div>
+                        <div class="product-info product-price"><?php echo "$".$productArray[$k]["price"]; ?></div>
+                        <div class="cart-action">
+                            <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
+                            <input type="submit" class="addbtn" value="add to cart"/>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        <?php
+            }
+        }
+        ?>
+    </div>
+</main>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/common/footer.php'?>
