0a8d4c2b247750f6d8e3aa0e33f883139b39cfda
Megan Hendrickson <dearmegs@gmail.com>
2020-05-13 12:27:17 -0500

    update database files

--- db/myDB.sql
+++ db/myDB.sql
@@ -1,21 +1,21 @@
-CREATE TABLE "Users" (
+CREATE TABLE "User" (
 	"userId" serial PRIMARY KEY NOT NULL UNIQUE,
 	"userName" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL
 );
 
 CREATE TABLE "Budget" (
 	"budgetId" serial PRIMARY KEY NOT NULL UNIQUE,
-	"userId" int references Users(userId) NOT NULL,
+	"userId" int references "User"("userId") NOT NULL,
 	"budgetName" varchar(255) NOT NULL,
 	"budgetAmount" int NOT NULL,
-	"created_at" datetime NOT NULL,
-;
+	"created_at" date NOT NULL
+);
 
 CREATE TABLE "Expense" (
 	"expenseId" serial PRIMARY KEY NOT NULL UNIQUE,
-	"budgetId" int references Budget(budgetId) NOT NULL,
+	"budgetId" int references "Budget"("budgetId") NOT NULL,
 	"expenseAmount" int NOT NULL,
 	"description" varchar(255) NOT NULL,
-	"created_at" datetime NOT NULL,
+	"created_at" date NOT NULL
 );
