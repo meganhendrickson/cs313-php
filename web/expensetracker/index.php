05180b60f1326ca0cc8a28ccbda376235cd74388
Megan Hendrickson <dearmegs@gmail.com>
2020-05-21 16:18:07 -0500

    db connection

--- web/expensetracker/index.php
+++ web/expensetracker/index.php
@@ -5,6 +5,9 @@
 // Create or access a Session
 session_start();
 
+// Get database connection file
+require_once("../library/functions.php");
+
 $action = filter_input(INPUT_POST, 'action');
     if($action == NULL){
         $action = filter_input(INPUT_GET, 'action');
@@ -13,6 +16,7 @@ $action = filter_input(INPUT_POST, 'action');
 
 switch ($action){
     case 'something':
+    exit;
     break;
 
     default:
