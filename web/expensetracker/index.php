90ab81ffe2d52a409b164229b57a910814ad5e86
Megan Hendrickson <dearmegs@gmail.com>
2020-05-21 17:39:56 -0500

    db connection

--- web/expensetracker/index.php
+++ web/expensetracker/index.php
@@ -1,29 +1,29 @@
 <?php
 //database connection
 function dbConnection() {
-  try
-  {
-    $dbUrl = getenv('DATABASE_URL');
-  
-    $dbOpts = parse_url($dbUrl);
-  
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-  
-    $dbConnection = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-  
-    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-    return $dbConnection;
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+    
+      $dbOpts = parse_url($dbUrl);
+    
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+    
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $db;
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    
 
   function getAllUsers(){
     $db = dbConnection();
