b5b21b93ec12d347c99cba194f62a815a6ee359d
Megan Hendrickson <dearmegs@gmail.com>
2020-05-21 13:05:59 -0500

    db connection

--- web/expensetracker/library/functions.php
+++ web/expensetracker/library/functions.php
@@ -1,25 +1,39 @@
 <?php
 //database connection
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+function dbConnection(){
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+  
+    $dbOpts = parse_url($dbUrl);
+  
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+  
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
 }
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
+
+function getAllUsers(){
+  $db = dbConnection();
+  $sql = 'SELECT * FROM User';
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $allUsers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $stmt->closeCursor();
+  return $allUsers;
 }
+
+echo print_r (getAllUsers());
 
 ?>
