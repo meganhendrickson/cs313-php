74ea589a2463c6982ea12b7b0667aa9e39cd633c
Megan Hendrickson <dearmegs@gmail.com>
2020-05-21 17:17:38 -0500

    db connection

--- web/expensetracker/index.php
+++ web/expensetracker/index.php
@@ -1,25 +1,46 @@
 <?php
-
-/* EXPENSE TRACKER CONTROLLER */
-
-// Create or access a Session
-session_start();
-
-// Get database connection file
-require_once $_SERVER['DOCUMENT_ROOT'].'/connections.php';
-
-$action = filter_input(INPUT_POST, 'action');
-    if($action == NULL){
-        $action = filter_input(INPUT_GET, 'action');
-    }
-
-
-switch ($action){
-    case 'something':
-    exit;
-    break;
-
-    default:
-        include ('view/dashboard.php');
+//database connection
+function dbConnect() {
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+  
+    $dbOpts = parse_url($dbUrl);
+  
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+  
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
 }
+
+//include header
+ob_start();
+include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php';
+$buffer = ob_get_contents();
+ob_end_clean();
+
+//set page title
+$title = "Dashboard";
+$buffer = preg_replace('/(<title>)(.*?)(<\/title>)/i', '$1' . $title . '$3', $buffer);
+echo $buffer;
+
 ?>
+
+<main>
+  <h1>Dashboard</h1>
+  
+</main>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php' ?>
