9085dc0fa41fa9bbf26b32c45c94c9692330c375
Megan Hendrickson <dearmegs@gmail.com>
2020-05-21 16:03:08 -0500

    db connection

--- web/expensetracker/library/functions.php
+++ web/expensetracker/library/functions.php
@@ -16,7 +16,7 @@ function dbConnect() {
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
   
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbConnect;
+    echo "Connected successfully";
   }
   catch (PDOException $ex)
   {
