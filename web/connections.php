24c81283973fe537bc9d2a341c528c019e100719
Megan Hendrickson <dearmegs@gmail.com>
2020-05-21 17:08:39 -0500

    db connection

---
+++ web/connections.php
@@ -0,0 +1,27 @@
+<?php
+//database connection
+function dbConnect() {
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
+  
+    $dbOpts = parse_url($dbUrl);
+  
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+  
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+}
+?>
