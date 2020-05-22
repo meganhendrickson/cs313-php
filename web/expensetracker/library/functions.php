5f1f4d980c341a973e9dd4ce1805da1070433010
Megan Hendrickson <dearmegs@gmail.com>
2020-05-22 10:20:21 -0500

    db connection

--- web/expensetracker/library/functions.php
+++ web/expensetracker/library/functions.php
@@ -7,24 +7,23 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/connections.php';
 
 function getAllClientBudgets($clientId){
   $db = dbConnection();
-  $sql = 'SELECT * FROM budget WHERE clientId';
+  $sql = 'SELECT * FROM budget WHERE clientId = :clientId';
   $stmt = $db->prepare($sql);
+  $stmt->bindValue(':clientId', $reviewId, PDO::PARAM_INT);
   $stmt->execute();
-  $clientBudget = $stmt->fetchAll();
+  $clientBudget = $stmt->fetchAll(PDO::FETCH_ASSOC);
   $stmt->closeCursor();
-  return $clientBudget;
+  return $clientBudgets;
 }
 
 // Build dashboard summary display
 function buildDashboardSummary(){
-  $clientBudgets = getAllClientBudgets();
+  $clientBudgets = getAllClientBudgets(1);
   $dash = "<div class='dashdisplay'>";
   foreach ($clientBudgets as $budget){
-    $budgetName=$budget['budgetName'];
-    $budgetAmount=$budget['budgetAmount'];
     $dash .= "<div class='budgetsummary'>";
-    $dash .= "<p>$budgetName</p>";
-    $dash .= "<p>&#36;$budgetAmount</p>";
+    $dash .= "<p>$budget[budgetName]</p>";
+    $dash .= "<p>&#36;$budget[budgetAmount]</p>";
     $dash .= "</div>";
   }
   $dash .= "</div>";
