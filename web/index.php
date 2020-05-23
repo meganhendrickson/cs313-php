7fab4da34f3cef54acc917f42847a215d04f3581
Megan Hendrickson <dearmegs@gmail.com>
2020-05-23 00:07:29 -0500

    switch to scss

--- web/index.php
+++ web/index.php
@@ -13,8 +13,8 @@
         <h1>Well, Hello There!</h1>
         <p>Allow me to introduce myself: I'm a designer, a web developer-in-training, and a chocolate lover.
             I like to perfect every detail, put puzzles together, laugh at silly jokes, cry over bad design,
-            and take walks in the sunshine. I'm still figuring out what I'm doing with my life. But hey,
-            nice to meet you! I'm Megan.
+            and take walks in the sunshine. I'm still figuring out what I'm doing with my life. But hey, I'm Megan
+            and it's nice to meet you!
         </p>
     </div>
     <div class="hello-image">
