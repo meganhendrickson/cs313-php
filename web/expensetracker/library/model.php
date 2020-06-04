a4318cb61d6d25810b4d6036db29c62048a95b4d
Megan Hendrickson <dearmegs@gmail.com>
2020-06-03 23:52:42 -0500

    working on add expense

--- web/expensetracker/library/model.php
+++ web/expensetracker/library/model.php
@@ -49,12 +49,9 @@ function getBudgetExpenses($budgetId){
 }
 
 function addExpense($budgetId, $expenseAmount, $expenseDescr, $created_at){
-  echo $budgetId;
-        echo $expenseAmount;
-        echo $expenseDescr;
-        echo $created_at;
   $db = dbConnection();
-  $sql = 'INSERT INTO expense(budgetid, expenseamount, expensedescr, created_at) VALUES(:budgetid, :expenseamount, :expensedescr, :created_at)';
+  $sql = 'INSERT INTO expense (budgetid, expenseamount, expensedescr, created_at) 
+          VALUES (:budgetid, :expenseamount, :expensedescr, :created_at)';
   $stmt = $db->prepare($sql);
   $stmt->bindValue(':budgetid', $budgetId);
   $stmt->bindValue(':expenseamount', $expenseAmount);
@@ -64,6 +61,5 @@ function addExpense($budgetId, $expenseAmount, $expenseDescr, $created_at){
   $rowsChanged = $stmt->rowCount();
   $stmt->closeCursor();
   return $rowsChanged;
-  echo $rowsChanged;
 }
 ?>
