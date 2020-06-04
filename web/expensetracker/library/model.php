cabf0f6c85e172d64e08a4541a62e2855ada2c50
Megan Hendrickson <dearmegs@gmail.com>
2020-06-03 23:37:29 -0500

    working on add expense

--- web/expensetracker/library/model.php
+++ web/expensetracker/library/model.php
@@ -58,9 +58,12 @@ function addExpense($budgetId, $expenseAmount, $expenseDescr, $created_at){
   $stmt->bindValue(':expenseamount', $expenseAmount);
   $stmt->bindValue(':expensedescr', $expenseDescr);
   $stmt->bindValue(':created_at', $created_at);
+  echo bindvalues;
   $stmt->execute();
+  echo execute;
   $rowsChanged = $stmt->rowCount();
   $stmt->closeCursor();
   return $rowsChanged;
+  echo $rowsChanged;
 }
 ?>
