0e476d69a8f48db253ea890a9f7ba7419b5f1893
Megan Hendrickson <dearmegs@gmail.com>
2020-06-05 22:17:47 -0500

    figure out what is wrong with the syntax

--- web/expensetracker/library/model.php
+++ web/expensetracker/library/model.php
@@ -89,7 +89,7 @@ function addBudget($clientId, $budgetName, $budgetAmount, $created_at){
 
 function updateBudget($budgetId, $budgetName, $budgetAmount, $created_at){
   $db = dbConnection();
-  $sql = 'UPDATE budget SET budgetname = :budgetname, budgetamount = :budgetamount, created_at = :created_at) WHERE budgetid = :budgetid';
+  $sql = 'UPDATE budget SET budgetname = ":budgetname", budgetamount = ":budgetamount", created_at = ":created_at") WHERE budgetid = :budgetid';
   $stmt = $db->prepare($sql);
   $stmt = bindValue(':budgetid', $budgetId);
   $stmt = bindValue(':budgetname', $budgetName);
